efd5606ad70a84cc46fbfd4a5a9cb93b43d7c409
abhishek-odiya <abhishekodiya@outlook.com>
2025-04-01 23:33:09 +0530

    BST

---
+++ Binary_Search_Tree/BST.java
@@ -0,0 +1,18 @@
+package DSA.Binary_Search_Tree;
+
+
+// BTS is Same as Binary Tree
+//In BTS left subtree has small Nodes then root node, Left < Root
+//In BTS right subtree has large Nodes then root node, Right > Root
+//Left & Right subtree are also BST with N-o duplicates
+
+//Inorder Traversal of BST hives a sorted sequence L-Root-R
+
+
+public class BST {
+    public static void main(String[] args) {
+
+
+
+    }
+}
